8849e351d2a366099cdb405331c3160b470721c9
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-11-15 01:00:21 -0300

    Front quase finalizado, tema trocado e novas paginas inseridas

--- ListaDeContatos/Controllers/ContatoController.cs
+++ ListaDeContatos/Controllers/ContatoController.cs
@@ -8,5 +8,17 @@ namespace ListaDeContatos.Controllers
         {
             return View();
         }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
     }
 }
